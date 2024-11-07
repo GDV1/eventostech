c5517b27fb6044d4a698dcdb7813185e67e2ccb3
Gabriel Dias Vieira <gabriel.dias@solutis.com.br>
2024-11-07 12:04:12 -0300

    feat: add repositories

---
+++ api/src/main/java/com/eventostech/api/repositories/EventRepository.java
@@ -0,0 +1,11 @@
+package com.eventostech.api.repositories;
+
+import com.eventostech.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+
+
+}
